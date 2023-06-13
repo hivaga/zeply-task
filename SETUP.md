669c0928ffe5b55d4ad60535c13215e8ca585a11
Aleksander Hadzhimitov <aleksandar.hadzhimitov@fyorin.com>
2023-06-13 14:46:39 +0200

    [Feature] Material UI library [Feature] Adding Documentation

---
+++ SETUP.md
@@ -0,0 +1,12 @@
+
+#### Creating NX monorepo
+`npx create-nx-workspace@latest`
+  * Select React
+  * Select Webpack
+  * Select SCSS
+
+#### Adding NX to package.json script
+`"nx": "nx"`
+
+#### Adding Material UI Library
+`npm install @mui/material @emotion/react @emotion/styled`
--- apps/blockchain/src/app/app.tsx
+++ apps/blockchain/src/app/app.tsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
+import {Button} from "@mui/material";
 
 export function App() {
   return (
     <div>
       <h1 className={styles.header}>Blockchain Application</h1>
+      <Button variant={'outlined'}>Hello world</Button>
     </div>
   );
 }
